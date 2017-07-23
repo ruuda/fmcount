0902629b59c939c0007781ce5ee49f5522676920
Ruud van Asseldonk <dev@veniogames.com>
2017-07-23 18:59:06 +0200

    Try to port the Config.GetUri function to F#
    It compiles, but I have no clue whether this is idiomatic F# or not.

--- Main.fs
+++ Main.fs
@@ -1,8 +1,8 @@
 open System
 
 type Config = {
-  ApiKey: string;
-  Username: string;
+  ApiKey: string
+  Username: string
 }
 
 type Scrobble = {
@@ -15,6 +15,24 @@ type Scrobble = {
   Time: DateTime;
   Loved: bool;
 }
+
+type Page = {
+  TotalPages: int;
+  Scrobbles: Scrobble[];
+}
+
+let getUri (config: Config) (limit: int) (page: int) : Uri =
+  let baseUri = "https://ws.audioscrobbler.com/2.0"
+  let query = [
+        "method=user.getrecenttracks"
+        "user=" + config.Username
+        "api_key=" + config.ApiKey
+        "format=json"
+        "extended=1"
+        "limit=" + limit.ToString()
+        "page=" + page.ToString()
+      ]
+  new Uri(baseUri) // TODO: include query part.
 
 [<EntryPoint>]
 let main argv =
--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-Main.exe: Main.cs
-	mcs -reference:System.Json Main.cs
+Main.exe: Main.fs
+	fsharpc --reference:System.Json Main.fs
+
+# Main.exe: Main.cs
+#	mcs -reference:System.Json Main.cs
 
 run: Main.exe
 	mono Main.exe
